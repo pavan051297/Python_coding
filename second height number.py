21241e0bcf72cd2ad018174291f9da37482f5ca9
sekha <pavan.buchipalli05@gmail.com>
2022-12-30 18:53:50 +0530

    new count code

---
+++ second height number.py
@@ -0,0 +1,12 @@
+ a = [3,2,4,5,865,90,78,32,9]
+# g =0
+# c= 0
+# for i in range(len(a) -1):
+#     if a[i] > g:
+#         g = a[i]
+#
+# print(g)
+# for j in range(len(a) - 1):
+#     if a[j] > c and a[j] != g :
+#         c = a[j]
+# print(c)
